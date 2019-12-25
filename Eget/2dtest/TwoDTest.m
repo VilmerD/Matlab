4cfc489631836b3279c36b6c4139305fe6f366d2
VilmerD <vilmer.dahlberg@gmail.com>
2019-12-25 13:24:45 +0100

    Poisson

--- Eget/2dtest/TwoDTest.m
+++ Eget/2dtest/TwoDTest.m
@@ -1,22 +1,53 @@
 %% Initializing
-N = 120;
-xx = linspace(0,1,N+2); xinner = xx(2:end-1);
-yy = xx';                yinner = xinner';
-[X,Y] = meshgrid(xinner,yinner);
-
+N = 100;
+xx = linspace(0,1,N);
+yy = xx';
+[X,Y] = meshgrid(xx,yy);
 %% Matrix
 s = (mod(1:(N^2-1),N)~=0);
-super = double(s);
+superRow = double(s);
 
-M = sparse(-2*diag(ones(1,N^2)) + diag(super,1) + diag(super,-1) + diag(ones(1,N^2-N),N) + diag(ones(1,N^2-N), -N));
+super = spdiags([1 superRow]', 1, N^2, N^2);
+sub = spdiags([superRow 1]', -1, N^2, N^2);
+superduper = spdiags(ones(N^2,1), N, N^2, N^2);
+suberduper = spdiags(ones(N^2,1), -N, N^2, N^2);
+diagonal = -4*speye(N^2);
+
+M = -N^2*(super + superduper + sub + suberduper + diagonal);
 %% Initial function 
-f = @(x,y) sin(3*pi*y).^2.*y.*x.*sin(3*pi*x).^2;
-fm = f(xinner,yinner);              % Matrix
-finner = fm(:);                     % Vector
+f = @(x,y) 1000*exp(-100*((x-0.5).^2 + (y-0.5).^2));
+xmesh = X(:);
 
+ymesh = Y(:);
+
+fm = f(xmesh,ymesh);
+%% Boundary matricies
+
+westC = (mod(1:N^2,N) == 1)';
+westM = spdiags(westC, 0, N^2, N^2);
+
+northC = (1:N^2 < N+1)';
+northM = spdiags(northC, 0, N^2, N^2);
+
+eastC = (mod(1:N^2,N) == 0)';
+eastM = spdiags(eastC, 0, N^2, N^2);
+
+southC = (1:N^2 > N^2 - N)';
+southM = spdiags(southC, 0, N^2, N^2);
+
+%% Boundary Values
+alpha = [0 1/12 0 0]; beta = [1 0 0 0]; gamma = [1 1 0 0];
+beta = beta.*(-1).^(0:3);
+z = find(abs(alpha) + abs(beta) == 0);
+alpha(z) = ones(1, length(z));
+
+B = 2.*gamma./(alpha - N*beta); Bt = B.*(alpha+N*beta)/2;
+BndryF = (westC*B(1) + eastC*B(2) + northC*B(3) + southC*B(4))*N^2;
+BndryM = (westM*Bt(1) + eastM*Bt(2) + northM*Bt(3) + southM*Bt(4))*N^2;
 %% Lösning
-y = M\finner;
+y = sparse(M + BndryM)\(fm + BndryF);
 ym = reshape(y, [N,N]);
-ym = [zeros(1,N+2); zeros(N,1) ym zeros(N,1); zeros(1,N+2)];
-[X,Y] = meshgrid(xx,yy);
 mesh(X,Y,ym);
+xlabel("x");
+ylabel("y");
+shg
