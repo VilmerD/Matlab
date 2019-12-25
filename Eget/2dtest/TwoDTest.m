98f71ad038b624d31dfd7cba0fbfbcb6358fdb58
VilmerD <vilmer.dahlberg@gmail.com>
2019-12-25 18:54:36 +0100

    Boundary färdiga

--- Eget/2dtest/TwoDTest.m
+++ Eget/2dtest/TwoDTest.m
@@ -1,5 +1,5 @@
 %% Initializing
-N = 100;
+N = 400;
 xx = linspace(0,1,N);
 yy = xx';
 [X,Y] = meshgrid(xx,yy);
@@ -15,34 +15,39 @@ diagonal = -4*speye(N^2);
 
 M = -N^2*(super + superduper + sub + suberduper + diagonal);
 %% Initial function 
-f = @(x,y) 1000*exp(-100*((x-0.5).^2 + (y-0.5).^2));
+f = @(x,y) 10*exp(-100*(x-y).^2);
 xmesh = X(:);
 
 ymesh = Y(:);
 
 fm = f(xmesh,ymesh);
 %% Boundary matricies
-
-westC = (mod(1:N^2,N) == 1)';
+westC = sparse((mod(1:N^2,N) == 1)');
 westM = spdiags(westC, 0, N^2, N^2);
 
-northC = (1:N^2 < N+1)';
+northC = sparse((1:N^2 < N+1)');
 northM = spdiags(northC, 0, N^2, N^2);
 
-eastC = (mod(1:N^2,N) == 0)';
+eastC = sparse((mod(1:N^2,N) == 0)');
 eastM = spdiags(eastC, 0, N^2, N^2);
 
-southC = (1:N^2 > N^2 - N)';
+southC = sparse((1:N^2 > N^2 - N)');
 southM = spdiags(southC, 0, N^2, N^2);
-
 %% Boundary Values
-alpha = [0 1/12 0 0]; beta = [1 0 0 0]; gamma = [1 1 0 0];
+alpha = [0 1 0 0]; beta = [1 1 0 0];    %0x 1x y0 y1 
 beta = beta.*(-1).^(0:3);
+
+g1 = @(x) exp(sin(3*pi*x));
+g2 = @(x) exp(-100*(x-0.5).^2);
+g3 = @(x) 0;
+g4 = @(x) 0;
+
 z = find(abs(alpha) + abs(beta) == 0);
 alpha(z) = ones(1, length(z));
 
-B = 2.*gamma./(alpha - N*beta); Bt = B.*(alpha+N*beta)/2;
-BndryF = (westC*B(1) + eastC*B(2) + northC*B(3) + southC*B(4))*N^2;
+B = 2./(alpha - N*beta); Bt = B.*(alpha+N*beta)/2;                          %   -||-
+
+BndryF = (g1(xmesh).*westC*B(1) + g2(xmesh).*eastC*B(2) + g3(ymesh).*northC*B(3) + g4(ymesh).*southC*B(4))*N^2;
 BndryM = (westM*Bt(1) + eastM*Bt(2) + northM*Bt(3) + southM*Bt(4))*N^2;
 %% Lösning
 y = sparse(M + BndryM)\(fm + BndryF);
